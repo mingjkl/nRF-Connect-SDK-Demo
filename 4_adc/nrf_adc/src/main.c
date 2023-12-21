38af0e65e4fc8718ee34930f4f198f9f5727ac30
emmovo <mingjkl@live.com>
2023-12-21 16:13:17 +0800

    添加 nrf_adc demo 未验证

---
+++ 4_adc/nrf_adc/src/main.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include "zephyr/logging/log.h"
+#include "nrfx_saadc.h"
+#include <zephyr/kernel.h>
+
+
+LOG_MODULE_REGISTER(adc, LOG_LEVEL_DBG);
+
+
+
+#define CHANNEL_COUNT 1
+
+static nrfx_saadc_channel_t single_channel = NRFX_SAADC_DEFAULT_CHANNEL_SE(NRF_SAADC_INPUT_AIN0, 0);
+static nrf_saadc_value_t buffer[CHANNEL_COUNT];
+
+int adc_init(void)
+{
+	nrfx_err_t err;
+	err = nrfx_saadc_init(NRFX_SAADC_DEFAULT_CONFIG_IRQ_PRIORITY);
+	err = nrfx_saadc_channel_config(&single_channel);
+	uint32_t channels_mask = nrfx_saadc_channels_configured_get();
+	err = nrfx_saadc_simple_mode_set(channels_mask, NRF_SAADC_RESOLUTION_12BIT, NRF_SAADC_OVERSAMPLE_DISABLED, NULL);
+	err = nrfx_saadc_buffer_set(buffer, CHANNEL_COUNT);
+}
+
+int adc_value_get(void)
+{
+	nrfx_err_t err;
+	
+	err = nrfx_saadc_offset_calibrate(NULL);
+	err = nrfx_saadc_mode_trigger();
+	return buffer[0];
+}
+
+int main(void)
+{
+	LOG_INF("ADC Demo %s\n", CONFIG_BOARD);
+	adc_init();
+	
+	while (1)
+	{
+		LOG_INF("ADC value: %d\n", adc_value_get());
+		k_msleep(100);
+	}
+	
+	return 0;
+}
