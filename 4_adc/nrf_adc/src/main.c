b9d50f511058733c62a553ac9d79bc0bba323615
emmovo <46583123+mingjkl@users.noreply.github.com>
2023-12-24 23:18:58 +0800

    完成nrf adc代码验证

--- 4_adc/nrf_adc/src/main.c
+++ 4_adc/nrf_adc/src/main.c
@@ -12,11 +12,9 @@
 
 LOG_MODULE_REGISTER(adc, LOG_LEVEL_DBG);
 
-
-
 #define CHANNEL_COUNT 1
 
-static nrfx_saadc_channel_t single_channel = NRFX_SAADC_DEFAULT_CHANNEL_SE(NRF_SAADC_INPUT_AIN0, 0);
+static nrfx_saadc_channel_t single_channel = NRFX_SAADC_DEFAULT_CHANNEL_SE(NRF_SAADC_INPUT_AIN3, 0);
 static nrf_saadc_value_t buffer[CHANNEL_COUNT];
 
 int adc_init(void)
@@ -25,6 +23,9 @@ int adc_init(void)
 	err = nrfx_saadc_init(NRFX_SAADC_DEFAULT_CONFIG_IRQ_PRIORITY);
 	err = nrfx_saadc_channel_config(&single_channel);
 	uint32_t channels_mask = nrfx_saadc_channels_configured_get();
+
+	LOG_INF("ADC channels mask: %d\n", channels_mask);
+	
 	err = nrfx_saadc_simple_mode_set(channels_mask, NRF_SAADC_RESOLUTION_12BIT, NRF_SAADC_OVERSAMPLE_DISABLED, NULL);
 	err = nrfx_saadc_buffer_set(buffer, CHANNEL_COUNT);
 }
@@ -38,6 +39,11 @@ int adc_value_get(void)
 	return buffer[0];
 }
 
+int adc_to_voltage(int adc_value)
+{
+	return (adc_value * 3600) / 4096;
+}
+
 int main(void)
 {
 	LOG_INF("ADC Demo %s\n", CONFIG_BOARD);
@@ -45,8 +51,8 @@ int main(void)
 	
 	while (1)
 	{
-		LOG_INF("ADC value: %d\n", adc_value_get());
-		k_msleep(100);
+		printk("vol=%d\r\n", adc_to_voltage(adc_value_get()));
+		k_msleep(10);
 	}
 	
 	return 0;
